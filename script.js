ec204c940819b0459cb1b819751674b0149c0de7
RAUSHAN <mcstar6262@gmail.com>
2026-01-27 03:04:53 +0530

    fully functional bubble sort

--- script.js
+++ script.js
@@ -1,57 +1,88 @@
 console.log("connected");
 let array = [];
+let steps = [];
 let n;
+let isanimating = false;
+function fullreset() {
+    array = [];
+    document.getElementById("array-container").innerHTML = "";
+    steps = [];
+}
+function reset() {
+    steps = [];
+}
 
 function start() {
-    n = Number(document.getElementById("inputnum").value);
-    if (n > 0) {
-        createArray();
-        renderBars();
+    if (isanimating == false) {
+        isanimating = true;
+        array = [];
+        document.getElementById("array-container").innerHTML = "";
+        reset();
+        n = Number(document.getElementById("inputnum").value);
+        if (n > 0) {
+            console.log("n is " + n);
+            createArray();
+            console.log("Array created ");
+            renderBars();
+        }
+        else {
+            alert("error ");
+        }
+        isanimating = false;
     }
-    else {
-        alert("error ");
-    }
+
 }
 
 function createArray() {
-    array = [];
     for (let a = 0; a < n; a++) {
         array.push(Math.floor(Math.random() * 100));
     }
 }
 
 function renderBars() {
-    document.getElementById("array-container").innerHTML = "";
-
+    console.log("Redering started ");
     for (let i = 0; i < array.length; i++) {
         var bar = document.createElement('div');
         bar.id = 'bar' + i;
         bar.className = 'blocks';
         document.getElementById('array-container').appendChild(bar);
         bar.style.height = (array[i] + '%');
-        bar.style.width = (1 / n + '%');
-        bar.style.borderWidth = getRandomColor();
+        bar.style.width = (1 / n * 100 + '%');
+        bar.style.borderWidth = "2px";
+        bar.style.borderColor = "Black";
+        bar.style.borderStyle = "Solid";
+        bar.style.backgroundColor = "blue";
     }
+    console.log("redering done ");
 }
 
 
 
-let steps = [];
+
 function bubblesort() {
-    let len = array.length;
-    while (len > 0) {
-        let temp = 0;
-        while (temp < len - 1) {
-            steps.push(["compare", temp, temp + 1]);
-            if (array[temp] > array[temp + 1]) {
-                steps.push(["swap", temp, temp + 1]);
-                [array[temp], array[temp + 1]] = [array[temp + 1], array[temp]];
+    if (isanimating == false) {
+        reset();
+        isanimating = true;
+        console.log("sorting ");
+        let len = array.length;
+        while (len > 0) {
+            let temp = 0;
+            while (temp < len - 1) {
+                steps.push(["compare", temp, temp + 1]);
+                if (array[temp] > array[temp + 1]) {
+                    steps.push(["swap", temp, temp + 1]);
+                    [array[temp], array[temp + 1]] = [array[temp + 1], array[temp]];
+                }
+                temp++;
             }
-            temp++;
+            len--;
         }
-        len--;
+        console.log("sorted ");
+        console.log(steps);
+        animateArray(0);
+
     }
-    animateArray(0);
+
 }
 
 function animateArray(i) {
@@ -61,24 +92,32 @@ function animateArray(i) {
         first.style.borderWidth = '5px';
         second.style.borderWidth = '5px';
         if (steps[i][0] === "swap") {
-            swap(i);
+            swap(first, second);
         }
 
-        first.style.borderWidth = '2px';
-        second.style.borderWidth = '2px';
 
         setTimeout(function () {
+            first.style.borderWidth = '2px';
+            second.style.borderWidth = '2px';
+            first.style.backgroundColor = "blue";
+            second.style.backgroundColor = "blue";
             animateArray(i + 1);
-        }, 500);
+        }, 1);
+
+    }
+    if (i == steps.length) {
+        isanimating = false;
     }
 }
 
-function swap(i) {
+function swap(first, second) {
     {
         first.style.backgroundColor = "Red";
         second.style.backgroundColor = "Purple";
         let temp = first.style.height;
         first.style.height = second.style.height;
         second.style.height = temp;
+
     }
 }
+
